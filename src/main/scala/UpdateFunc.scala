09d3c5fd2bf8edfd0bc8eb92d772d33298dd1fe7
Julaiti Alafate <jalafate@gmail.com>
2016-12-24 19:38:50 -0800

    added UpdateFunc

---
+++ src/main/scala/UpdateFunc.scala
@@ -0,0 +1,11 @@
+package sparkboost
+
+import org.apache.spark.rdd.RDD
+
+object UpdateFunc {
+    def adaboostUpdate(instances: RDD[Instance], node: SplitterNode) = {
+    }
+
+    def logitboostUpdate(instances: RDD[Instance], node: SplitterNode) = {
+    }
+}
