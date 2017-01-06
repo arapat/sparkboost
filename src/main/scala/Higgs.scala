0b68aa70c9774e428d85024731cdded144c03ea0
Julaiti Alafate <jalafate@gmail.com>
2017-01-05 23:34:48 -0800

    added test for Higgs data

---
+++ src/main/scala/Higgs.scala
@@ -0,0 +1,20 @@
+package sparkboost
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object Higgs {
+    type Instance = (Int, Vector[Double], Double)
+
+    def main(args: Array[String]) {
+        val conf = new SparkConf().setMaster("local[2]")
+        val sc = new SparkContext(conf)
+        val data = sc.textFile(args(0))
+                     .map {line => line.split(",").map(_.trim.toDouble)}
+        val rdd = data.map {t => (t.head, t.tail.toVector)}
+                      .map {t => ((t._1 + t._1 - 1.0).toInt, t._2, 1.0)}
+                      .cache()
+        sc.stop()
+    }
+}
