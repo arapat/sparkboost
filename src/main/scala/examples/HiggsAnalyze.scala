e3242d3de06fa8179d237a55c9090f1fc186691f
Julaiti Alafate <jalafate@gmail.com>
2017-01-18 21:43:18 -0800

    serialized the result into a binary file

--- src/main/scala/examples/HiggsAnalyze.scala
+++ src/main/scala/examples/HiggsAnalyze.scala
@@ -1,5 +1,7 @@
 package sparkboost.examples
 
+import java.io._
+
 import collection.mutable.ListBuffer
 
 import org.apache.spark.SparkContext
@@ -15,7 +17,8 @@ object HiggsAdaboostAnalyze extends Comparison {
     args(1) - file path to the training data
     args(2) - file path to the testing data
     args(3) - File path where the model is saved
-    args(4) - which algorithm to use - 1 for AdaBoost, 2 for LogitBoost
+    args(4) - which algorithm to use: 1 for AdaBoost, 2 for LogitBoost
+    args(5) - early stop: 0 for no early stop, k for processing first k nodes only
     */
     def main(args: Array[String]) {
         def getNewPredict(inst: Instance, node: SplitterNode) = {
@@ -27,10 +30,10 @@ object HiggsAdaboostAnalyze extends Comparison {
             })
         }
 
-        if (args.size != 5) {
+        if (args.size != 6) {
             println(
-                "Please provide four arguments: master url, training data path, " +
-                "testing data path, model file path, algorithm ID."
+                "Please provide six arguments: master url, training data path, " +
+                "testing data path, model file path, algorithm ID, early stop index."
             )
             return
         }
@@ -57,9 +60,12 @@ object HiggsAdaboostAnalyze extends Comparison {
         var trainError = ListBuffer[Double]()
         var testError = ListBuffer[Double]()
         var ec = ListBuffer(train.count.toDouble)
+        var trainWeights = ListBuffer(train.map(_.w).collect.toList)
         var iters = 0
-        for (node <- nodes) {
+        val slice = if (args(5).toInt > 0) args(5).toInt else nodes.size
+        for (node <- nodes.take(slice)) {
             iters = iters + 1
+            println("Iteration " + iters)
             train = updateFunc(train, node).cache()
             test = updateFunc(test, node).cache()
             if (iters % 25 == 0) {
@@ -79,16 +85,28 @@ object HiggsAdaboostAnalyze extends Comparison {
             val wsq: Double = weights.map {x => x * x}
                                      .reduce(_ + _)
             ec += ((wsum * wsum).toDouble / wsq)
+            trainWeights += train.map(_.w).collect.toList
         }
 
-        def join(array : ListBuffer[Double]) = {
+        def join(array : List[Double]) = {
             array.tail.foldLeft(array.head.toString)((s, k) => s + ", " + k)
         }
+        /*
         println("Training error:")
-        println(join(trainError))
+        println(join(trainError.toList))
         println("Test error:")
-        println(join(testError))
+        println(join(testError.toList))
         println("Effective count:")
-        println(join(ec))
+        println(join(ec.toList))
+        println("Weights: " + trainWeights.size)
+        for (ws <- trainWeights) {
+            println(join(ws))
+        }
+        */
+        val oos = new ObjectOutputStream(new FileOutputStream("./result.bin"))
+        oos.writeObject((trainError.toList, testError.toList,
+                         ec.toList, trainWeights.toList))
+        oos.close()
+        println("Done.")
     }
 }
