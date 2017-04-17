00c05872601804d26b7aa9661d8f71c2aea86d45
Julaiti Alafate <jalafate@gmail.com>
2017-04-17 00:48:57 -0700

    working on seq logo visualization

---
+++ ipynb_src/seq_logo.py
@@ -0,0 +1,92 @@
+from operator import itemgetter
+import seaborn
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-ticks')
+from matplotlib import transforms
+import matplotlib.patheffects
+import numpy as np
+
+COLOR_SCHEME = {'G': 'orange', 
+                'A': 'red', 
+                'C': 'blue', 
+                'T': 'darkgreen'}
+
+class Scale(matplotlib.patheffects.RendererBase):
+    def __init__(self, sx, sy=None):
+        self._sx = sx
+        self._sy = sy
+
+    def draw_path(self, renderer, gc, tpath, affine, rgbFace):
+        affine = affine.identity().scale(self._sx, self._sy)+affine
+        renderer.draw_path(gc, tpath, affine, rgbFace)
+
+
+def draw_logo(all_scores):
+    fig = plt.figure()
+    fig.set_size_inches(len(all_scores), 2.5)
+    ax = fig.add_subplot(111)
+    ax.set_xticks(range(len(all_scores)))
+
+    xshift = 0
+    trans_offset = transforms.offset_copy(
+        ax.transAxes, 
+        fig=fig, 
+        x=0, 
+        y=0, 
+        units='points'
+    )
+
+    for scores in all_scores:
+        yshift = 0
+        for base, score in scores:
+            txt = ax.text(0, 
+                          0, 
+                          base, 
+                          transform=trans_offset,
+                          fontsize=80, 
+                          color=COLOR_SCHEME[base],
+                          weight='bold',
+                          ha='center',
+                          family='sans-serif')
+            txt.set_clip_on(False) 
+            txt.set_path_effects([Scale(1.0, score)])
+            fig.canvas.draw()
+            window_ext = txt.get_window_extent(txt._renderer)
+            yshift = window_ext.height * score
+            trans_offset = transforms.offset_copy(txt._transform, fig=fig, y=yshift, units='points')
+        xshift += window_ext.width
+        trans_offset = transforms.offset_copy(ax.transAxes, fig=fig, x=xshift, units='points')
+
+
+    ax.set_yticks(range(0,3))
+
+
+    seaborn.despine(ax=ax, offset=30, trim=True)
+    ax.set_xticklabels(range(1,len(all_scores)+1), rotation=90)
+    ax.set_yticklabels(np.arange(0,3,1))
+
+    plt.show()
+
+
+def getScores(filepath, leftBound=None, rightBound=None):
+    scoresMap = {}
+
+    with open(filepath) as f:
+        for line in f:
+            index, depth, score, name = line.strip().split(", ")
+            pos, base = int(name[3:-1]), name[-1]
+            if pos not in scoresMap:
+                scoresMap[pos] = {}
+            scoresMap[pos][base] = scoresMap[pos].get(base, 0.0) + abs(float(score))
+
+    scores = []
+    rng = range(0, max(scoresMap.keys()) + 1)
+    if leftBound is not None and rightBound is not None:
+        rng = range(leftBound, rightBound + 1)
+    for i in rng:  # range(0, max(scoresMap.keys()) + 1):
+        if i not in scoresMap:
+            scoresMap[i] = {}
+        for c in ['A', 'C', 'G', 'T']:
+            scoresMap[i][c] = scoresMap[i].get(c, 0.0) + 0.0
+        scores.append(sorted(scoresMap[i].items(), key=itemgetter(1)))
+    return scores
